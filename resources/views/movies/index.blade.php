e4a7a3eeab5f8e64f5e96419cd1c872ceb21de64
TariqAyman <tariq.ayman94@gmail.com>
2020-08-29 18:01:59 +0200

    [ View ] create index page for list movies

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container mx-auto px-4 pt-16">
+        <div class="popular-movies">
+            <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Movies</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+                @foreach ($movies as $movie)
+                    @include('components.movie-card',$movie)
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
