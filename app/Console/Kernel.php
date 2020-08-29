55a8770372421dcc50570bea461b576189361bde
TariqAyman <tariq.ayman94@gmail.com>
2020-08-29 03:10:23 +0200

    [ Kernel ] add fetchThemoviedbData to schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('fetchThemoviedbData')->cron('0 */'.env('INTERVAL_HOURS',2).' * * *');
     }
 
     /**
